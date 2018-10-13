25d6fb39c81fa0e4dafbea349671f4a3cb8a0ab5
Kayla Ford <kaylaf9616@gmail.com>
2018-10-13 02:21:56 -0400

    added react router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import  Map  from './components/map';
 import reducers from './reducers';
-
+import { Route } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+
+const Routes = () => (
+
+  <HashRouter>
+    <div className = "app">
+    <Route exact path="/explore" component={Map} />
+    </div>
+  </HashRouter>
+
+);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Routes />
   </Provider>,
  document.getElementById('root'));
 
